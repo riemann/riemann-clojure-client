28b6a881687e8d22aa4ec6674972541a20c55f7d
Aphyr <aphyr@aphyr.com>
2012-08-05 15:42:56 -0700

    Add boundary repo.

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ A clojure client for [Riemann](http://aphyr.github.com/riemann)
 
 ## Usage
 
+For riemann-java-client, add the boundary repo to your project.clj:
+```clojure
+:repositories {
+  "boundary-site" "http://maven.boundary.com/artifactory/repo"
+}
+```
+
 ``` clojure
 (use 'riemann.client)
 (def c (tcp-client :host "1.2.3.4"))
--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject riemann-clojure-client "0.0.3"
+(defproject riemann-clojure-client "0.0.4-SNAPSHOT"
   :description "Clojure client for the Riemann monitoring syste,"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"boundary-site" "http://maven.boundary.com/artifactory/repo"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [com.aphyr/riemann-java-client "0.0.3"]])
