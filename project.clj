f2779f715f2be3c031d6f4f865e1c2f4ccd93370
Aphyr <aphyr@aphyr.com>
2012-12-03 14:15:40 -0800

    Version 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann-clojure-client "0.0.6-SNAPSHOT"
+(defproject riemann-clojure-client "0.0.6"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
