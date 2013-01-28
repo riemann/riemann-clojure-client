3e1a36023a07a35faca436b11c56a89f501c19f9
Aphyr <aphyr@aphyr.com>
2013-01-28 10:26:51 -0800

    Switch to RiemannThreadedClient by default.

--- src/riemann/client.clj
+++ src/riemann/client.clj
@@ -17,7 +17,9 @@
   dropped connection once before giving up. They're backed by a
   RiemannRetryingTcpClient."
 
-  (:import [com.aphyr.riemann.client RiemannRetryingTcpClient 
+  (:import [com.aphyr.riemann.client RiemannThreadedClient
+                                     RiemannRetryingTcpClient
+                                     RiemannTcpClient 
                                      RiemannUDPClient 
                                      AbstractRiemannClient]
            [java.net InetSocketAddress]
@@ -41,7 +43,20 @@
        (.sendEventsWithAck client events)
        (.sendEvents client events)))))
 
-(defn tcp-client 
+(defn tcp-client
+  "Creates a new threaded TCP client. Example:
+
+  (tcp-client)
+  (tcp-client :host \"foo\" :port 5555)"
+  [& { :keys [^String host ^Integer port]
+       :or {port 5555
+            host "localhost"}
+       :as opts}]
+  (doto (RiemannThreadedClient.
+          (RiemannTcpClient. (InetSocketAddress. host port)))
+    (.connect)))
+
+(defn retrying-tcp-client
   "Create a new TCP client. Example:
 
   (tcp-client)
--- test/riemann/client_test.clj
+++ test/riemann/client_test.clj
@@ -5,10 +5,13 @@
 (deftest send-query
          (let [c (tcp-client)
                e {:service "test" :state "ok"}]
+           (try
              (send-event c {:service "test" :state "ok"})
              (let [e1 (first (query c "service = \"test\" and state = \"ok\""))]
                (is (= (:service e) (:service e1)))
                (is (= (:state e) (:state e1)))
                (is (float? (:ttl e1)))
-               (is (integer? (:time e1))))))
+               (is (integer? (:time e1))))
+             (finally
+               (close-client c)))))
 
