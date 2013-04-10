8f9017ecebf35f120e5178dd89deb31f08667c70
Aphyr <aphyr@aphyr.com>
2013-04-10 09:57:48 -0700

    Depend on java client 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [com.aphyr/riemann-java-client "0.2.0"]])
+                 [com.aphyr/riemann-java-client "0.2.1-SNAPSHOT"]])
