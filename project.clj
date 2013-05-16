492df8add538515bf237381f29f1b0a823f7e689
Kyle Kingsbury <kingsbury@factual.com>
2013-05-16 15:10:41 -0700

    Version 0.2.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject riemann-clojure-client "0.2.2"
+(defproject riemann-clojure-client "0.2.3-SNAPSHOT"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [com.aphyr/riemann-java-client "0.2.2"]])
+                 [com.aphyr/riemann-java-client "0.2.3-SNAPSHOT"]])
