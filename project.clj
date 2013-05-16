b19b661ccc4033280c1ac5bf4fb3c3bf71fa3469
Kyle Kingsbury <kingsbury@factual.com>
2013-05-16 14:41:47 -0700

    Clojure 1.5.0

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [com.aphyr/riemann-java-client "0.2.2-SNAPSHOT"]])
