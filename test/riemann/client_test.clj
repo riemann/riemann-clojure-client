af0e7786294ef658cd1762afa146be4b5550c4f4
Kyle Kingsbury <aphyr@aphyr.com>
2014-04-25 15:13:09 -0700

    tcp-client & udp-client accept either option maps or kwargs.

--- test/riemann/client_test.clj
+++ test/riemann/client_test.clj
@@ -4,7 +4,7 @@
         clojure.test))
 
 (deftest async-test
-         (let [c (tcp-client)
+         (let [c (tcp-client :host "localhost")
                e {:service "async-test" :foo "hi there"}]
            (try
              (let [res (async-send-event c e)]
@@ -19,7 +19,7 @@
                (close-client c)))))
 
 (deftest send-query
-         (let [c (tcp-client)
+         (let [c (tcp-client {:host "localhost"})
                e {:service "test" :state "ok"}]
            (try
              (send-event c e)
