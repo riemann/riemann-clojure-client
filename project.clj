75a5afe16167e9406c9be5ab17c9216d38957176
Kyle Kingsbury <aphyr@aphyr.com>
2014-12-08 13:46:52 -0800

    0.2.12; java client 0.2.11

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject riemann-clojure-client "0.2.12-SNAPSHOT"
+(defproject riemann-clojure-client "0.2.12"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.3"]
+  :dependencies [[org.clojure/tools.logging "0.2.3"]
                  [less-awful-ssl "0.1.1"]
-                 [com.aphyr/riemann-java-client "0.2.10"]])
+                 [com.aphyr/riemann-java-client "0.2.11"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}})
