26aefb7a73be0e46a769c306340ea2ebaa80a58f
Kyle Kingsbury <aphyr@aphyr.com>
2014-12-08 13:48:43 -0800

    0.2.13-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann-clojure-client "0.2.12"
+(defproject riemann-clojure-client "0.2.13-SNAPSHOT"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
