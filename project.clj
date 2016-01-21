b086c9815e82fdf078be874181c59283ffc6895e
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-01-21 13:54:02 +0100

    cleanup dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject riemann-clojure-client "0.4.1"
+(defproject riemann-clojure-client "0.4.2"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.logging "0.2.3"]
-                 [less-awful-ssl "1.0.0"]
-                 [com.aphyr/riemann-java-client "0.4.0"]]
+  :dependencies [[less-awful-ssl "1.0.0"]
+                 [com.aphyr/riemann-java-client "0.4.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :bench {:dependencies [[org.clojure/clojure "1.6.0"]
                                     [criterium "0.4.1"]]
