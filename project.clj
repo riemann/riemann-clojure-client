c5dec91c1dbbfa51d6ee1dee5f51d0c5c61dea9e
James Turnbull <james@lovedthanlost.net>
2017-01-24 08:08:24 -0500

    Updated Java client to 0.4.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject riemann-clojure-client "0.4.3-SNAPSHOT"
+(defproject riemann-clojure-client "0.4.3"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[less-awful-ssl "1.0.0"]
-                 [io.riemann/riemann-java-client "0.4.2"]]
+                 [io.riemann/riemann-java-client "0.4.3"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :bench {:dependencies [[org.clojure/clojure "1.8.0"]
                                     [criterium "0.4.1"]]
