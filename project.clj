46ce157748f3218373ababaaba732ab35bcf7b20
Jean-Baptiste Besselat <jean-baptiste.besselat@linkfluence.net>
2017-01-26 15:14:38 +0100

    [FEATURE] add possibility to specify source address

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[less-awful-ssl "1.0.0"]
-                 [io.riemann/riemann-java-client "0.4.3"]]
+                 [io.riemann/riemann-java-client "0.4.3-LINKFLUENCE"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :bench {:dependencies [[org.clojure/clojure "1.8.0"]
                                     [criterium "0.4.1"]]
