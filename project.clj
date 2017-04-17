ad923a518097747f592fcc1d637f86fc22fd137b
James Turnbull <james@lovedthanlost.net>
2017-04-17 16:32:48 -0400

    Updates
    * Updated riemann-java-client to 0.4.5
    * Added ability to specify source address

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject riemann-clojure-client "0.4.4"
+(defproject riemann-clojure-client "0.4.5"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/aphyr/riemann-clojure-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[less-awful-ssl "1.0.0"]
-                 [io.riemann/riemann-java-client "0.4.3"]]
+                 [io.riemann/riemann-java-client "0.4.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :bench {:dependencies [[org.clojure/clojure "1.8.0"]
                                     [criterium "0.4.1"]]
