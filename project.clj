4e580526cd7d0a932b7fb3a80d0997fbf45b0533
James Turnbull <james@lovedthanlost.net>
2022-03-13 21:03:33 +1100

    Updated to 0.5.5-SNAPSHOT
    Signed-off-by: James Turnbull <james@lovedthanlost.net>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann-clojure-client "0.5.4"
+(defproject riemann-clojure-client "0.5.5-SNAPSHOT"
   :description "Clojure client for the Riemann monitoring system"
   :url "https://github.com/riemann/riemann-clojure-client"
   :license {:name "Eclipse Public License"
